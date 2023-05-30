8af969c46237866ac8924ec37d5ef51aba3ef94c
robbryandev <robbryandev@gmail.com>
2023-05-30 12:01:12 -0700

    Add getSearchParams procedure

--- config.nim
+++ config.nim
@@ -1,12 +1,13 @@
 import std/json
 
 type Config* = object
-  sources*, titles*, keywords*, exclude*: seq[string]
+  sources*, titles*, keywords*, match*, exclude*: seq[string]
 
 proc newConfig*(configJson: JsonNode): Config =
   result = Config(
     sources: configJson["sources"].to(seq[string]),
     titles: configJson["titles"].to(seq[string]),
     keywords: configJson["keywords"].to(seq[string]),
+    match: configJson["match"].to(seq[string]),
     exclude: configJson["exclude"].to(seq[string])
   )
--- jobs.nim
+++ jobs.nim
@@ -1,7 +1,38 @@
-import json
+import std/[json, strutils, strformat]
 import "./config.nim"
 
-let configJson = parseFile("./config.json")
-let jobConfig = configJson.newConfig()
+let configJson: JsonNode = parseFile("./config.json")
+let jobConfig: Config = configJson.newConfig()
 
-echo $jobConfig
+# get search parameters to send to the linkBuilder
+proc getSearchParams(config: Config): string =
+  # Build a list of strings that will be joined together to form the search query.
+  var linkParts: seq[string] = @[]
+
+  # Add the sources to the query.
+  linkParts.add(config.sources.join(" | "))
+
+  # Add the titles to the query.
+  let titles: string = config.titles.join(" | ")
+  linkParts.add(fmt"({titles})")
+
+  # Add the keywords to the query.
+  let keywords: string = config.keywords.join(" | ")
+  linkParts.add(fmt"({keywords})")
+
+  # Add exact match keywords to the query.
+  var match: seq[string] = @[]
+  for m in config.match:
+    match.add("\"" & m & "\"")
+  linkParts.add(match.join(" "))
+
+  # Add the excluded keywords to the query.
+  var exclude: seq[string] = @[]
+  for ex in config.exclude:
+    exclude.add(fmt"-{ex}")
+  linkParts.add(exclude.join(" "))
+
+  # Join the list of strings together to form the search query.
+  return linkParts.join(" ")
+
+echo getSearchParams(jobConfig)
